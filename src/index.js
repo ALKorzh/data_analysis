4d89d2cb5c3306747ba086153f40ff610afafd40
ALKorzh <alexkorzhwork@gmail.com>
2024-03-20 22:14:49 +0300

    created an array with only valid data

--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ const records = generateData(RECORDS_N)
 
 console.table(records)
 console.log(prices)
+
+function validateTable() {
+  const validate = []
+  for (let i = 0; i < RECORDS_N; i++) {
+    let flag = true
+    for (let key in records[i]) {
+      if (!(records[i][key] === '')) {
+        if (records[i].count < 0) {
+          flag = false
+        }
+      } else {
+        flag = false
+        continue
+      }
+    }
+    if (flag) {
+      validate.push(records[i])
+    }
+  }
+  return validate
+}
+console.log(validateTable())
